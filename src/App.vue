62c75e46c12dae3368dabe83a1cd89cac381ac3a
ytf2y <178884773@qq.com>
2019-09-28 17:20:33 +0800

    commit first

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="showFlag=true">点击显示toast组件</button>
+    <!-- <Toast v-show="showFlag"  title="自定义的title" :show="showFlag" @update:show=""> -->
+    <Toast v-show="showFlag" :btns="['取消','确定']" title="自定义的title" :show.sync="showFlag" >
+      <div style="color:red;">我是自定义的组件内容</div>
+      <div style="color:red;">我是自定义的组件内容</div>
+    </Toast>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Toast from './components/Toast.vue'
 
 export default {
   name: 'app',
+  data:function()
+  {
+    return {
+      showFlag:false//控制toast组件的显示隐藏
+    }
+  },
   components: {
-    HelloWorld
+    Toast
   }
 }
 </script>
@@ -21,8 +31,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Toast.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="toast-page">
+    <div class="body">
+      <div class="title">{{title}}</div>
+      <div class="content">
+        <slot></slot>  <!-- 放置一个插槽(占位) -->
+      </div>
+      <div class="btns"> <span  @click="$emit('hidetoast')">取消</span><span class="confirm" @click="$emit('update:show',false)">确定</span> </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'toast',
+  props: {
+    btns: Array,
+    show: Boolean,
+    title: String  /* propTypes */
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .toast-page{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, .5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .toast-page .body{
+    line-height: 40px;
+    background-color: white;
+    padding: 50px;
+    border-radius: 10px;
+  }
+  .toast-page .body .title{
+    text-align: center;
+    font-weight: bold;
+  }
+  .toast-page .body .btns{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .toast-page .body .btns .confirm{
+    color: red;
+  }
+
+</style>
